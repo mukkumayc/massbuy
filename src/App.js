53f161502380f79b8b6826a0fd89849413cc6b65
mukkumayc <st063775@student.spbu.ru>
2020-10-01 17:11:05 +0300

    Add authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Routes from "./components/Routes";
+import Config from "./config";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setAuthenticating] = useState(true);
+  const appProps = { isAuthenticated, userHasAuthenticated };
+
+  useEffect(() => {
+    fetch(Config.serverUrl + "/checktoken", {
+      credentials: "include",
+    })
+      .then((res) => {
+        userHasAuthenticated(res.ok);
+        setAuthenticating(false);
+      })
+      .catch((e) => {
+        console.error(e);
+        setAuthenticating(false);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <Routes />
+      {!isAuthenticating && <Routes appProps={appProps} />}
     </div>
   );
 }
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import AuthenticatedRoute from "./AuthenticatedRoute";
+import UnauthenticatedRoute from "./UnauthenticatedRoute";
 import Home from "../containers/Home";
 import NotFound from "../containers/NotFound";
+import Login from "../containers/Login";
 
 const Routes = ({ appProps }) => {
   return (
     <Switch>
-      <Route path="/" exact component={Home} appProps={appProps} />
+      <AuthenticatedRoute path="/" exact component={Home} appProps={appProps} />
+      <UnauthenticatedRoute
+        path="/login"
+        exact
+        component={Login}
+        appProps={appProps}
+      />
       <Route component={NotFound} />
     </Switch>
   );
