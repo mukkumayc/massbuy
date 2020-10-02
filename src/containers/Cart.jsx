949f19b1d596261daf18101af6112fc46e1004cb
mukkumayc <st063775@student.spbu.ru>
2020-10-02 15:34:53 +0300

    Add cart page template

---
+++ src/containers/Cart.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Cart = (props) => {
+  return <Container className="cart">Cart page template</Container>;
+};
+
+export default Cart;
