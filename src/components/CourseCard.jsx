497cbdd6674ef55ecf52d5949cbe595903de05a3
mukkumayc <st063775@student.spbu.ru>
2020-10-12 13:01:24 +0300

    Change minimum card size

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 
 const CourseCard = (props) => {
   return (
-    <Card border="primary" style={{ margin: "0.2rem" }}>
+    <Card
+      border="primary"
+      className="course-card"
+      style={{ "min-width": "300px", margin: "0.2em", "min-height": "276px" }}
+    >
       <Link to={"/course/" + props.id}>
         <Card.Body>
           <Card.Title>{props.course_name}</Card.Title>
