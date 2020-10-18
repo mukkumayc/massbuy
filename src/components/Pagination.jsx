e8500230942aa65202ece1d31a536758f692f951
MukkuMayc <st063775@student.spbu.ru>
2020-10-18 16:41:08 +0300

    Fix pagination problems

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,14 +4,23 @@ import { LinkContainer } from "react-router-bootstrap";
 
 const createItems = (pagesNum) => {
   let locationSearch = new URLSearchParams(window.location.search);
+  let publicUrlPathname;
+  try {
+    publicUrlPathname = new URL(process.env.PUBLIC_URL).pathname;
+  } catch (err) {
+    publicUrlPathname = process.env.PUBLIC_URL;
+  }
   let items = [];
   if (pagesNum < 100) {
     for (let number = 1; number <= pagesNum; ++number) {
       locationSearch.set("page", number);
+      let pathname = window.location.pathname.startsWith(publicUrlPathname)
+        ? window.location.pathname.substr(publicUrlPathname.length)
+        : window.location.pathname;
       items.push(
         <LinkContainer
           key={number}
-          to={`${window.location.pathname}?${locationSearch.toString()}`}
+          to={`${pathname}?${locationSearch.toString()}`}
         >
           <Pagination.Item>{number}</Pagination.Item>
         </LinkContainer>
