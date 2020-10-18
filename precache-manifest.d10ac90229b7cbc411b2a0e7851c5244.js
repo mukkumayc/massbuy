42990188f6bc633df14894965932a7ab3ad5fe7a
MukkuMayc <st063775@student.spbu.ru>
2020-10-18 15:00:01 +0300

    Updates

---
+++ precache-manifest.d10ac90229b7cbc411b2a0e7851c5244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c1198193b4e05042a0bdd617c61c52",
+    "url": "/massbuy/index.html"
+  },
+  {
+    "revision": "d5e7c940e9c23d538f39",
+    "url": "/massbuy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "143f7a7066d4e46adf28",
+    "url": "/massbuy/static/css/main.d6e38a34.chunk.css"
+  },
+  {
+    "revision": "d5e7c940e9c23d538f39",
+    "url": "/massbuy/static/js/2.7fc44271.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/massbuy/static/js/2.7fc44271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "143f7a7066d4e46adf28",
+    "url": "/massbuy/static/js/main.9ace6bda.chunk.js"
+  },
+  {
+    "revision": "54b652a7805cdd2d4d24",
+    "url": "/massbuy/static/js/runtime-main.37de7a8c.js"
+  }
+]);
