3fa849b561c07a784cb94e1d70d88b3bd9e40d39
MukkuMayc <st063775@student.spbu.ru>
2020-10-18 14:51:49 +0300

    Add basename parameter to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
